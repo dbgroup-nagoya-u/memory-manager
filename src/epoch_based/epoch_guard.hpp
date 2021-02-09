ccfcc699f221f90474bb75ac26e7dee4c491ce83
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2021-02-09 15:34:56 +0900

    add epoch guard class

---
+++ src/epoch_based/epoch_guard.hpp
@@ -0,0 +1,40 @@
+// Copyright (c) Database Group, Nagoya University. All rights reserved.
+// Licensed under the MIT license.
+
+#pragma once
+
+#include "garbage_collection/epoch_manager.hpp"
+
+namespace gc
+{
+class EpochGuard
+{
+ private:
+  /*################################################################################################
+   * Internal member variables
+   *##############################################################################################*/
+
+  size_t current_epoch_;
+
+  EpochManager* epoch_manger_;
+
+ public:
+  /*################################################################################################
+   * Public constructors/destructors
+   *##############################################################################################*/
+
+  explicit EpochGuard(EpochManager* epoch_manger)
+  {
+    current_epoch_ = epoch_manger->EnterEpoch();
+    epoch_manger_ = epoch_manger;
+  }
+
+  ~EpochGuard() { epoch_manger_->LeaveEpoch(current_epoch_); }
+
+  EpochGuard(const EpochGuard&) = delete;
+  EpochGuard& operator=(const EpochGuard&) = delete;
+  EpochGuard(EpochGuard&&) = default;
+  EpochGuard& operator=(EpochGuard&&) = default;
+};
+
+}  // namespace gc
