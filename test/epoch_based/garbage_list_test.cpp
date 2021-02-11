3576e71c0f56b0bfdcd5f14af0c3215d19b4e6a7
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2021-02-11 15:06:39 +0900

    add explanations

--- test/epoch_based/garbage_list_test.cpp
+++ test/epoch_based/garbage_list_test.cpp
@@ -30,13 +30,16 @@ class GarbageListFixture : public ::testing::Test
 
 TEST_F(GarbageListFixture, Construct_ArgTargetPointer_MemberVariableCorrectlyInitialized)
 {
+  // create GC targets
   const auto target_ptr_1 = new size_t{1};
   const auto target_ptr_2 = new size_t{2};
 
+  // create a garbage item
   const auto garbage_tail = new GarbageList{target_ptr_1};
 
   EXPECT_EQ(nullptr, garbage_tail->Next());
 
+  // create a garbage list
   const auto garbage_head = GarbageList{target_ptr_2, garbage_tail};
 
   EXPECT_EQ(garbage_tail, garbage_head.Next());
@@ -44,13 +47,16 @@ TEST_F(GarbageListFixture, Construct_ArgTargetPointer_MemberVariableCorrectlyIni
 
 TEST_F(GarbageListFixture, SetNext_SwapNullptrForNextGarbage_ReferenceCorrectNext)
 {
+  // create GC targets
   const auto target_ptr_1 = new size_t{1};
   const auto target_ptr_2 = new size_t{2};
 
+  // create a garbage item
   auto garbage_head = GarbageList{target_ptr_1};
 
   EXPECT_EQ(nullptr, garbage_head.Next());
 
+  // expand a garbage item to a list
   const auto garbage_tail = new GarbageList{target_ptr_2};
   garbage_head.SetNext(garbage_tail);
 
@@ -60,14 +66,19 @@ TEST_F(GarbageListFixture, SetNext_SwapNullptrForNextGarbage_ReferenceCorrectNex
 
 TEST_F(GarbageListFixture, Destruct_SetOne_AllocatedValueFreed)
 {
+  // create GC targets
   const auto target_ptr_1 = new size_t{1};
   const auto target_ptr_2 = new size_t{2};
 
   {
+    // create a garbage list
     const auto garbage_tail = new GarbageList{target_ptr_1};
     const auto garbage_head = GarbageList{target_ptr_2, garbage_tail};
+
+    // a garbage list delete GC targets when it leaves this scope
   }
 
+  // this check is danger. these lines may fail due to freed memory status
   EXPECT_NE(1, *target_ptr_1);
   EXPECT_NE(2, *target_ptr_2);
 }
