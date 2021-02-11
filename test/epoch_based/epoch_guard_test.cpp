0de0ddea3d4f7d035401bedb13d09190906ee9d9
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2021-02-11 13:29:23 +0900

    add test for Destructor in EpochGuard

--- test/epoch_based/epoch_guard_test.cpp
+++ test/epoch_based/epoch_guard_test.cpp
@@ -40,4 +40,29 @@ TEST_F(EpochGuardFixture, Construct_ArgEpochManager_MemberVariableCorrectlyIniti
   EXPECT_EQ(0, epoch);
 }
 
+TEST_F(EpochGuardFixture, Destruct_HundredThreads_DestructorSuccessfullyLeaveEpoch)
+{
+  // keep an initial epoch
+  const auto begin_epoch = epoch_manager.GetCurrentEpoch();
+
+  // create epoch-guards with 100 threads in 10 epochs
+  for (size_t loop = 0; loop < 10; ++loop) {
+    std::vector<std::thread> threads;
+    for (size_t count = 0; count < 100; ++count) {
+      threads.emplace_back([em = &epoch_manager] { const auto guard = EpochGuard{em}; });
+    }
+    for (auto &&thread : threads) {
+      thread.join();
+    }
+    epoch_manager.ForwardEpoch();
+  }
+
+  // check all the threads leave corresponding epochs by a destructor
+  const auto end_epoch = epoch_manager.GetCurrentEpoch();
+  const auto [freeable_begin, freeable_end] = epoch_manager.ListFreeableEpoch();
+
+  EXPECT_EQ(begin_epoch, freeable_begin);
+  EXPECT_EQ(end_epoch, freeable_end);
+}
+
 }  // namespace gc::epoch
