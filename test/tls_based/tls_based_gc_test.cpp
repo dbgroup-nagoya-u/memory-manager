26ba26f81b33ba9e3e0c452dbfc4c1d8279f928c
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2021-02-24 21:50:57 +0900

    add multi-thread test for TLSBasedGC

--- test/tls_based/tls_based_gc_test.cpp
+++ test/tls_based/tls_based_gc_test.cpp
@@ -53,7 +53,7 @@ TEST_F(TLSBasedGCFixture, Construct_GCStopped_MemberVariablesCorrectlyInitialize
 
 TEST_F(TLSBasedGCFixture, Destruct_SingleThread_GarbagesCorrectlyFreed)
 {
-  constexpr size_t kLoopNum = 1E3;
+  constexpr size_t kLoopNum = 5E3;
 
   // keep garbage targets
   std::vector<std::weak_ptr<size_t>> target_weak_ptrs;
@@ -73,8 +73,54 @@ TEST_F(TLSBasedGCFixture, Destruct_SingleThread_GarbagesCorrectlyFreed)
       }
     };
 
-    auto t = std::thread{f};
-    t.join();
+    std::thread{f}.join();
+
+    // GC deletes all targets when it leaves this scope
+  }
+
+  // check there is no referece to target pointers
+  for (auto &&target_weak : target_weak_ptrs) {
+    EXPECT_EQ(0, target_weak.use_count());
+  }
+}
+
+TEST_F(TLSBasedGCFixture, Destruct_MultiThreads_GarbagesCorrectlyFreed)
+{
+  constexpr size_t kLoopNum = 5E3;
+  constexpr size_t kThreadNum = 100;
+
+  // keep garbage targets
+  std::vector<std::weak_ptr<size_t>> target_weak_ptrs;
+
+  // register garbages to GC
+  {
+    auto gc = TLSBasedGC<std::shared_ptr<size_t>>{kGCInterval};
+
+    auto f = [&gc](std::promise<std::vector<std::weak_ptr<size_t>>> p) {
+      std::vector<std::weak_ptr<size_t>> weak_ptrs;
+
+      for (size_t loop = 0; loop < kLoopNum; ++loop) {
+        const auto guard = gc.CreateEpochGuard();
+        const auto target_shared = new std::shared_ptr<size_t>(new size_t{loop});
+        weak_ptrs.emplace_back(*target_shared);
+        gc.AddGarbage(target_shared);
+
+        std::this_thread::sleep_for(std::chrono::microseconds(50));
+      }
+
+      p.set_value(weak_ptrs);
+    };
+
+    std::vector<std::future<std::vector<std::weak_ptr<size_t>>>> futures;
+    for (size_t count = 0; count < kThreadNum; ++count) {
+      std::promise<std::vector<std::weak_ptr<size_t>>> p;
+      futures.emplace_back(p.get_future());
+      std::thread{f, std::move(p)}.detach();
+    }
+    for (auto &&future : futures) {
+      auto weak_ptrs = future.get();
+      target_weak_ptrs.insert(target_weak_ptrs.end(), weak_ptrs.begin(), weak_ptrs.end());
+    }
 
     // GC deletes all targets when it leaves this scope
   }
