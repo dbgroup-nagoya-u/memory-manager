3dc702d8f9912c575d1cd98d681a2c8acce757fa
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2021-02-24 11:17:50 +0900

    add destructor test for epoch manager

--- test/tls_based/epoch_manager_test.cpp
+++ test/tls_based/epoch_manager_test.cpp
@@ -5,7 +5,9 @@
 
 #include <gtest/gtest.h>
 
+#include <memory>
 #include <thread>
+#include <vector>
 
 namespace dbgroup::gc::tls
 {
@@ -32,6 +34,52 @@ TEST_F(EpochManagerFixture, Construct_NoArgs_MemberVariablesCorrectlyInitialized
   auto manager = EpochManager{};
 
   EXPECT_EQ(0, manager.GetCurrentEpoch());
+}
+
+TEST_F(EpochManagerFixture, Destruct_AfterAddOneEpoch_AddedEpochFreed)
+{
+  std::weak_ptr<Epoch> epoch_reference;
+
+  {
+    auto manager = EpochManager{};
+    const auto epoch = std::make_shared<Epoch>(manager.GetCurrentEpoch());
+
+    // register an epoch to a manager
+    manager.AddEpoch(epoch);
+
+    // keep the reference to an epoch
+    epoch_reference = epoch;
+
+    // the created epoch is freed here because all the shared_ptr are out of scope
+  }
+
+  EXPECT_EQ(0, epoch_reference.use_count());
+}
+
+TEST_F(EpochManagerFixture, Destruct_AfterAddTenEpochs_AddedEpochsFreed)
+{
+  constexpr auto kLoopNum = 10UL;
+
+  std::vector<std::weak_ptr<Epoch>> epoch_references;
+
+  {
+    auto manager = EpochManager{};
+
+    for (size_t count = 0; count < kLoopNum; ++count) {
+      // register an epoch to a manager
+      const auto epoch = std::make_shared<Epoch>(manager.GetCurrentEpoch());
+      manager.AddEpoch(epoch);
+
+      // keep the reference to an epoch
+      epoch_references.emplace_back(epoch);
+    }
+
+    // the created epochs are freed here because all the shared_ptr are out of scope
+  }
+
+  for (auto &&epoch_reference : epoch_references) {
+    EXPECT_EQ(0, epoch_reference.use_count());
+  }
 }
 
 TEST_F(EpochManagerFixture, ForwardEpoch_ForwardTenTimes_CurrentEpochCorrectlyUpdated)
