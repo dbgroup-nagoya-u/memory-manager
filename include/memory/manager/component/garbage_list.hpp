588c3e6fa242042ccf9dd4159326a95816485bcc
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2021-03-19 16:35:37 +0900

    WIP: modify garbage list to linked list

--- include/memory/manager/component/garbage_list.hpp
+++ include/memory/manager/component/garbage_list.hpp
@@ -3,6 +3,7 @@
 
 #pragma once
 
+#include <array>
 #include <atomic>
 #include <limits>
 #include <memory>
@@ -22,17 +23,13 @@ class GarbageList
    * Internal member variables
    *##############################################################################################*/
 
-  const size_t buffer_size_;
+  std::array<std::pair<size_t, T*>, kGarbageBufferSize> garbages_;
 
-  std::vector<std::pair<size_t, T*>> garbage_ring_buffer_;
+  std::atomic_size_t head_index_;
 
-  std::atomic_size_t begin_index_;
+  size_t tail_index_;
 
-  std::atomic_size_t end_index_;
-
-  std::atomic_size_t current_epoch_;
-
-  const size_t gc_interval_micro_;
+  std::atomic<GarbageList*> next_;
 
   MemoryKeeper* memory_keeper_;
 
@@ -41,48 +38,16 @@ class GarbageList
    * Public constructors/destructors
    *##############################################################################################*/
 
-  constexpr GarbageList()
-      : buffer_size_{0},
-        begin_index_{0},
-        end_index_{0},
-        current_epoch_{0},
-        gc_interval_micro_{std::numeric_limits<size_t>::max()},
-        memory_keeper_{nullptr}
+  constexpr GarbageList() : head_index_{0}, tail_index_{0}, next_{nullptr}, memory_keeper_{nullptr}
   {
   }
 
-  GarbageList(  //
-      const size_t buffer_size,
-      const size_t current_epoch,
-      const size_t gc_interval_micro,
-      MemoryKeeper* memory_keeper = nullptr)
-      : buffer_size_{buffer_size},
-        begin_index_{0},
-        end_index_{0},
-        current_epoch_{current_epoch},
-        gc_interval_micro_{gc_interval_micro},
-        memory_keeper_{memory_keeper}
+  explicit GarbageList(MemoryKeeper* memory_keeper)
+      : head_index_{0}, tail_index_{0}, next_{nullptr}, memory_keeper_{memory_keeper}
   {
-    garbage_ring_buffer_.reserve(buffer_size_);
-    for (size_t i = 0; i < buffer_size_; ++i) {
-      garbage_ring_buffer_.emplace_back(std::numeric_limits<size_t>::max(), nullptr);
-    }
   }
 
-  ~GarbageList()
-  {
-    const auto current_end = end_index_.load(mo_relax);
-    auto index = begin_index_.load(mo_relax);
-    while (index != current_end) {
-      auto [deleted_epoch, garbage] = garbage_ring_buffer_[index];
-      if (memory_keeper_ == nullptr) {
-        delete garbage;
-      } else {
-        memory_keeper_->ReturnPage(garbage);
-      }
-      index = (index == buffer_size_ - 1) ? 0 : index + 1;
-    }
-  }
+  ~GarbageList() = default;
 
   GarbageList(const GarbageList&) = delete;
   GarbageList& operator=(const GarbageList&) = delete;
@@ -90,74 +55,60 @@ class GarbageList
   GarbageList& operator=(GarbageList&&) = delete;
 
   /*################################################################################################
-   * Public getters/setters
-   *##############################################################################################*/
-
-  size_t
-  Size() const
-  {
-    const auto current_begin = begin_index_.load(mo_relax);
-    const auto current_end = end_index_.load(mo_relax);
-
-    if (current_begin <= current_end) {
-      return current_end - current_begin;
-    } else {
-      return buffer_size_ - (current_begin - current_end);
-    }
-  }
-
-  void
-  SetCurrentEpoch(const size_t current_epoch)
-  {
-    current_epoch_.store(current_epoch, mo_relax);
-  }
-
-  /*################################################################################################
    * Public utility functions
    *##############################################################################################*/
 
-  void
-  AddGarbage(const T* garbage)
+  GarbageList*
+  AddGarbage(  //
+      const size_t current_epoch,
+      const T* garbage)
   {
-    // reserve a garbage region
-    const auto current_end = end_index_.load(mo_relax);
-    const auto next_end = (current_end == buffer_size_ - 1) ? 0 : current_end + 1;
-    auto current_begin = begin_index_.load(mo_relax);
-    while (next_end == current_begin) {
-      // wait GC
-      std::this_thread::sleep_for(std::chrono::microseconds(gc_interval_micro_));
-      current_begin = begin_index_.load(mo_relax);
+    const auto current_head = head_index_.load(mo_relax);
+    garbages_[current_head] = {current_epoch, garbage};
+    head_index_.store(1, mo_relax);
+
+    if (current_head < kGarbageBufferSize - 1) {
+      return this;
+    } else {
+      const auto new_garbage_list = new GarbageList{memory_keeper_};
+      next_.store(new_garbage_list, mo_relax);
+      return new_garbage_list;
     }
-
-    // add garbage
-    garbage_ring_buffer_[current_end] = {current_epoch_.load(mo_relax), const_cast<T*>(garbage)};
-
-    // set incremented index
-    end_index_.store(next_end, mo_relax);
   }
 
-  void
-  Clear(const size_t protected_epoch)
+  static GarbageList*
+  Clear(  //
+      GarbageList* garbage_list,
+      const size_t protected_epoch)
   {
-    const auto current_end = end_index_.load(mo_relax);
+    const auto current_head = head_index_.load(mo_relax);
+    auto memory_keeper = garbage_list->memory_keeper_;
 
-    auto index = begin_index_.load(mo_relax);
-    while (index != current_end) {
-      auto [deleted_epoch, garbage] = garbage_ring_buffer_[index];
-      if (deleted_epoch < protected_epoch) {
-        garbage_ring_buffer_[index] = {std::numeric_limits<size_t>::max(), nullptr};
-        if (memory_keeper_ == nullptr) {
+    auto index = garbage_list->tail_index_;
+    for (; index < current_head; ++index) {
+      const auto [epoch, garbage] = garbage_list->garbages_[index];
+      if (epoch < protected_epoch) {
+        if (memory_keeper == nullptr) {
           delete garbage;
         } else {
-          memory_keeper_->ReturnPage(garbage);
+          memory_keeper->ReturnPage(garbage);
         }
       } else {
         break;
       }
-      index = (index == buffer_size_ - 1) ? 0 : index + 1;
     }
 
-    begin_index_.store(index, mo_relax);
+    if (index < kGarbageBufferSize) {
+      garbage_list->tail_index_ = index;
+      return garbage_list;
+    } else {
+      auto next_list = garbage_list->next_.load(mo_relax);
+      while (next_list == nullptr) {
+        next_list = garbage_list->next_.load(mo_relax);
+      }
+      delete garbage_list;
+      return GarbageList::Clear(next_list, protected_epoch);
+    }
   }
 };
 
